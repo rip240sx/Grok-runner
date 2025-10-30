7c09a8819701eb7841ed6d0fad43e6bdba8c4d52
rip240sx <rip240sx@gmail.com>
2025-10-30 16:30:08 -0500

    Update canvas style for full viewport display

--- app/page.tsx
+++ app/page.tsx
@@ -1193,8 +1193,8 @@ export default function Game() {
         </div>
       )}
 
-      <canvas ref={canvasRef} style={{ display: "block", width: "100%", height: "100%" }} />
-
+      <canvas ref={canvasRef} style={{display:"block",width:"100vw",height:"100vh",position:"fixed",top:0,left:0,margin:0,padding:0}} />
+     
       <div
         style={{
           position: "absolute",
