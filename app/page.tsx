a4db0dc2f2029e60c97e32eb81ff70b87d07b6f4
rip240sx <rip240sx@gmail.com>
2025-10-30 20:36:28 -0500

    Adjust canvas height to use calc for viewport

--- app/page.tsx
+++ app/page.tsx
@@ -1226,7 +1226,7 @@ export default function Game() {
         </div>
       )}
 
-      <canvas ref={canvasRef} style={{display:"block",width:"100vw",height:"100vh",position:"fixed",top:0,left:0,margin:0,padding:0}} />
+      <canvas ref={canvasRef} style={{display:"block",width:"100vw",height:"calc(var(--vh) * 100)",position:"fixed",top:0,left:0,margin:0,padding:0}} />
      
       <div
         style={{
